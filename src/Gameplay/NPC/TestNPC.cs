6457c713121065d7814e5518a4731c5ea349820c
lpopov1 <luben.popov@gmail.com>
2025-01-26 13:30:45 -0500

    Add smoothing and ledge boost to NPC movement

--- src/Gameplay/NPC/TestNPC.cs
+++ src/Gameplay/NPC/TestNPC.cs
@@ -3,6 +3,12 @@ using Godot;
 
 public partial class TestNPC : NPC
 {
+    [Export]
+    private float _SmoothingCoeff = 2.0F;
+    [Export]
+    private float _LedgeBoost = 1.0F;
+
+    private Vector3 _lastTargetVelocity = Vector3.Zero;
     // private MeshInstance3D _mesh;
     // private StandardMaterial3D _material;
 
@@ -24,11 +30,17 @@ public partial class TestNPC : NPC
             .GlobalPosition;
         setTargetPosition(playerPosition);
         Velocity = getTargetVelocity();
+        if (Velocity.Y > 0.1 && IsOnWall())
+        {
+            Velocity = new Vector3(Velocity.X, Velocity.Y + _LedgeBoost, Velocity.Z);
+        }
+        Velocity = _lastTargetVelocity.Lerp(Velocity, _SmoothingCoeff * (float)delta);
+        _lastTargetVelocity = Velocity;
         MoveAndSlide();
         var lookTarget = new Vector3(
-            GlobalPosition.X + Velocity.X,
+            GlobalPosition.X + _lastTargetVelocity.X,
             GlobalPosition.Y,
-            GlobalPosition.Z + Velocity.Z
+            GlobalPosition.Z + _lastTargetVelocity.Z
         );
         Transform = Transform.LookingAt(lookTarget, Vector3.Up);
     }
